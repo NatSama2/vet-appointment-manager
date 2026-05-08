42b72a002de281a44aed480161eced318fc0d46a
NatSama2 <vet.nat.c@gmail.com>
2026-05-08 02:00:03 -0400

    feat: inicializa frontend con Quasar

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted } from 'vue'
+import api from 'src/services/api'
+
+onMounted(async () => {
+  const res = await api.get('/citas')
+  console.log(res.data)
+})
+</script>
+
+<template>
+  <q-page class="flex flex-center">
+    <h4>VetCitas 🐾</h4>
+  </q-page>
+</template>
