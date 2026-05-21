11e00dc49411ed3c3578ea6c0493070390d5d2cf
NatSama2 <vet.nat.c@gmail.com>
2026-05-20 20:17:29 -0400

    feat(front): agregar ruteado del sidebar

---
+++ frontend/src/pages/MascotasPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="q-pa-md">
+    <h4>Mascotas</h4>
+  </div>
+</template>
---
+++ frontend/src/pages/NuevaCitaPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="q-pa-md">
+    <h4>las wenas citas</h4>
+  </div>
+</template>
